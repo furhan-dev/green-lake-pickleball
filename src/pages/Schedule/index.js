4da0592912fc85ef472d055a03df039076140565
Furhan Samani <contact@furhan.dev>
2023-03-14 22:36:16 -0700

    use chakra, make navbar

---
+++ src/pages/Schedule/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Box } from '@chakra-ui/react'
+
+function Schedule(props) {
+  return (
+    <Box p={4}>Schedule</Box>
+  )
+}
+
+Schedule.propTypes = {}
+
+export default Schedule
