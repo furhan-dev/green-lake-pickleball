81a78ce89ef35f3f25a0b00260a5e33d9926bd55
Furhan Samani <contact@furhan.dev>
2023-06-16 23:21:08 -0700

    get login tests passing

--- server/server.js
+++ server/server.js
@@ -1,32 +1,27 @@
 const express = require('express');
 const path = require('path');
 const { clog } = require('./middleware/clog');
-const db = require('./config/connection');
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 
-const app = express();
+const server = express();
 
 // Import custom middleware, "cLog"
-app.use(clog);
+server.use(clog);
 
 // Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  server.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
+server.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.use(routes);
+server.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`Server is listening on http://localhost:${PORT} 🚀`);
-  });
-});
+module.exports = server;
